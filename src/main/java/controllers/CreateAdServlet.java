cec301ccc0241c44a58b3557c3beb2c0d862007e
Sam Lansford <sam.j.lansford@gmail.com>
2021-04-14 09:25:44 -0500

    doing sessions-and-cookies

---
+++ src/main/java/controllers/CreateAdServlet.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import dao.DaoFactory;
+import models.Ad;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/ads/create")
+public class CreateAdServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/ads/create.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+        long userId = 1;
+        long id = 5;
+
+        Ad adToInsert = new Ad(id, userId, title, description);
+        System.out.println(adToInsert);
+        DaoFactory.getAdsDao().insert(adToInsert);
+
+        response.sendRedirect("/ads");
+
+    }
+}
--- src/main/webapp/ads/index.jsp
+++ src/main/webapp/ads/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: samlansford
@@ -8,9 +9,23 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <jsp:include page="/partials/head.jsp">
+        <jsp:param name="title" value="Viewing All The dao.Ads" />
+    </jsp:include>
 </head>
 <body>
+<jsp:include page="/partials/navbar.jsp" />
+
+<div class="container">
+    <h1>Here Are all the ads!</h1>
+
+    <c:forEach var="ad" items="${ads}">
+        <div class="col-md-6">
+            <h2>${ad.title}</h2>
+            <p>${ad.description}</p>
+        </div>
+    </c:forEach>
+</div>
 
 </body>
 </html>
