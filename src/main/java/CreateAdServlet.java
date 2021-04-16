088b6df4fa866d8abcfc97978de70af8ccb6d11c
Sam Lansford <sam.j.lansford@gmail.com>
2021-04-16 12:01:22 -0500

    doing jdbc

--- src/main/java/CreateAdServlet.java
+++ src/main/java/CreateAdServlet.java
@@ -7,18 +7,26 @@ import java.io.IOException;
 
 @WebServlet(name = "CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
-            .forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Ad ad = new Ad(
-            1, // for now we'll hardcode the user id
-            request.getParameter("title"),
-            request.getParameter("description")
-        );
-        DaoFactory.getAdsDao().insert(ad);
-        response.sendRedirect("/ads");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Ad adToBeCreated = new Ad();
+
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+
+        adToBeCreated.setTitle(title);
+        adToBeCreated.setDescription(description);
+        adToBeCreated.setUserId(1L);
+
+        long savedId = DaoFactory.getAdsDao().insert(adToBeCreated);
+        adToBeCreated.setId(savedId);
+
+        request.setAttribute("ad", adToBeCreated);
+        request.getRequestDispatcher("/WEB-INF/ads/show.jsp").forward(request, response);
+
     }
 }
---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,66 @@
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class MySQLAdsDao implements Ads {
+
+    Connection connection = null;
+
+    public MySQLAdsDao(Config config){
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Ad> all(){
+        List<Ad> ads = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM ads");
+            while(rs.next()){
+
+                ads.add(new Ad(
+                        rs.getLong("id"),
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                ));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ads;
+    }
+
+    @Override
+    public long insert(Ad adToBeSaved){
+        long newId = 0;
+
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "INSERT INTO ads (user_id, title, description) " +
+                    "VALUES("+adToBeSaved.getUserId()+", '"+adToBeSaved.getTitle()+"', '"+adToBeSaved.getDescription()+"')";
+            statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.getGeneratedKeys();
+            if(rs.next()) {
+                newId = rs.getLong(1);
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        return newId;
+    }
+}
